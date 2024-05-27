c352e29e1b57912e58d91218aaef7b022e9c00ef
LucasQuiroga27 <lucasquirogafp@gmail.com>
2024-05-26 21:11:15 -0500

    primer commit, creando el proyecto

---
+++ MarvelAppMVC/MarvelAppMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelAppMVC
+//
+//  Created by Lucas Quiroga on 26/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
