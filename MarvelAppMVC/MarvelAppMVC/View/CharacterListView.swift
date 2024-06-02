b17f765a4a4fdfe662790f7d4c787e068678e62b
LucasQuiroga27 <lucasquirogafp@gmail.com>
2024-06-02 18:48:26 -0500

    creando la tableView

---
+++ MarvelAppMVC/MarvelAppMVC/View/CharacterListView.swift
@@ -0,0 +1,40 @@
+//
+//  CharacterListView.swift
+//  MarvelAppMVC
+//
+//  Created by Lucas Quiroga on 2/06/24.
+//
+
+import Foundation
+import UIKit
+
+class CharacterListView: UIView {
+    
+    let charactersTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView(){
+        addSubview(charactersTableView)
+        
+        NSLayoutConstraint.activate([
+            charactersTableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            charactersTableView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            charactersTableView.topAnchor.constraint(equalTo: topAnchor),
+            charactersTableView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    
+}
